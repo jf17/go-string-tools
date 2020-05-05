c2c0e9aafb4084a250c83acd9bce1908db752dd6
Alex Alexandrovich <javafirst17@gmail.com>
2020-05-05 17:07:39 +0300

    Update stringtools.go

--- stringtools.go
+++ stringtools.go
@@ -2,9 +2,10 @@ package stringtools
 
 import "golang.org/x/text/encoding/charmap"
 
-func ConvertWindows1251ToUTF8(ba []uint8) string  {
+func ConvertWindows1251ToUTF8(str string) string  {
+	byteArr := []byte(str)
 	dec := charmap.Windows1251.NewDecoder()
-	out, _ := dec.Bytes(ba)
+	out, _ := dec.Bytes(byteArr)
 	return string(out)
 }
 
