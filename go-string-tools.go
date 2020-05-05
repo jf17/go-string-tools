fd153cc81d1b99ddbebb6a74ea6f213267c071db
Alex Alexandrovich <javafirst17@gmail.com>
2020-05-05 16:38:16 +0300

    Create go-string-tools.go

---
+++ go-string-tools.go
@@ -0,0 +1,10 @@
+package go-string-tools
+
+import "fmt"
+
+func ConvertWindows1251ToUTF8(ba []uint8) string  {
+	dec := charmap.Windows1251.NewDecoder()
+	out, _ := dec.Bytes(ba)
+	return string(out)
+}
+
